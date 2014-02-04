cd9140227919e392701172e31c9e02ad69a02de0
Albul Alexandr <albul@gamepoint.com.ua>
2014-02-04 17:22:26 +0200

    Refactoring

--- src/js/utils/collision.js
+++ src/js/utils/collision.js
@@ -1,9 +1,30 @@
+/**
+ * Copyright (c) 2011-2012 Alexandr Albul
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 define('utils.collision', function (app) {
 
+    // Public methods --------------------------------------------- */
     return {
 
         /**
-         * Проверяет на столкновение объекта с точькой
+         * Collision check the object with the point
+         * @param obj
+         * @param x x coordinate of the point
+         * @param y y coordinate of the point
+         * @return {boolean}
          */
         hitTestPoint: function (obj, x, y) {
             if (obj.x < x && obj.y < y
@@ -14,7 +35,5 @@ define('utils.collision', function (app) {
                 return false;
             }
         }
-
     }
-
 });
