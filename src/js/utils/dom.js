ea0b93df3152fcd959c1b6d7caa666d9adc0cede
Albul Alexandr <albul@gamepoint.com.ua>
2014-02-04 17:24:32 +0200

    Refactoring

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,8 +1,26 @@
+/**
+ * Copyright (c) 2011-2012 Alexandr Albul
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 define('utils.dom', function (app) {
 
+    // Public methods --------------------------------------------- */
     return {
+
         /**
-         * Возвращает глобальные координаты элемента на странице
+         * Returns the global coordinates of the element on the page
          * @param element HTMLElement
          * @return {Object}
          */
@@ -28,5 +46,4 @@ define('utils.dom', function (app) {
             return {x: x, y: y};
         }
     }
-
 });
